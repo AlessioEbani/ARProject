1a94c440318bedbaae750efb9cc1a8663b4f3b2d
Alessio Ebani <ebani.alessio@studenti-ittfedifermi.edu.it>
2022-06-30 11:43:42 +0200

    Snapper

--- DAR&D/Assets/Scripts/Snapper.cs
+++ DAR&D/Assets/Scripts/Snapper.cs
@@ -1,18 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class Snapper : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+[ExecuteInEditMode]
+public class Snapper : MonoBehaviour {
+    private Vector3Int size;
+
+    private void OnDrawGizmos() {
+        if (!Application.isPlaying) {
+            UpdateSize();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Start() {
+        UpdateSize();
+    }
+
+    void Update() {
+        SnapToGrid();
+    }
+
+    private void UpdateSize() {
+        var localScale = transform.localScale;
+        size = new Vector3Int(Mathf.CeilToInt(localScale.x), Mathf.CeilToInt(localScale.y), Mathf.CeilToInt(localScale.z));
+    }
+
+    private void SnapToGrid() {
+        var currentPosition = transform.position;
+        var posX = (size.x % 2 == 0) ? currentPosition.x - 0.5f : currentPosition.x;
+        posX = Mathf.RoundToInt(posX);
+        if (size.x % 2 == 0) {
+            posX += 0.5f;
+        }
         
+        float posY = Mathf.RoundToInt(currentPosition.y);
+        
+        float posZ = (size.z % 2 == 0) ? currentPosition.z - 0.5f : currentPosition.z;
+        posZ = Mathf.RoundToInt(posZ);
+        if (size.z % 2 == 0) {
+            posZ += 0.5f;
+        }
+        var position = new Vector3(posX,posY,posZ);
+        transform.position = position;
     }
 }
